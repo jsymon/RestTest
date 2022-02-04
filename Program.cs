63f4fdbb675e827033f220ae6e882d09542bbdf0
James Symon <jamessymon@ELE-21L.whiteways.tech>
2022-02-04 12:37:32 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace VSCodeTest
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+
+
+            var tStream = await streamTask;
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(tStream);
+
+            Console.WriteLine("Found Repositories:\n------------");
+            foreach (var repository in repositories)
+            {
+                Console.WriteLine(repository.name);
+            }
+        }
+
+
+
+
+        public class Repository
+        {
+            public string name { get; set; }
+
+        }
+
+    }
+
+
+}
